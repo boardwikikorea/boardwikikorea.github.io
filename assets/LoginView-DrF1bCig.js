83e55482ea6832972409c6ac3cb41ad1c3ca1689
DESKTOP-7EG9OS2\jinji <jinji0326@naver.com>
2026-02-06 10:18:15 +0900

    update github-page 2026-02-06 10:18:14

---
+++ assets/LoginView-DrF1bCig.js
@@ -0,0 +1,1 @@
+import{_ as f}from"./index-1UsboSlJ.js";export{f as default};
--- assets/profile-7DDwvAu_.js
+++ assets/profile-7DDwvAu_.js
@@ -1,4 +1,4 @@
-import{w as c,s as r}from"./index-CkkheZHq.js";async function o(i){return c(async()=>{var e;const{data:n,error:t}=await r.from("profile").select(`
+import{w as c,s as r}from"./index-1UsboSlJ.js";async function o(i){return c(async()=>{var e;const{data:n,error:t}=await r.from("profile").select(`
         nickname,
         full_name,
         birth_date,
