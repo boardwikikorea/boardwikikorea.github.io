312714458080c6234e0d854a7aeccf42b7e0c10e
DESKTOP-7EG9OS2\jinji <jinji0326@naver.com>
2026-02-20 12:20:28 +0900

    update

--- salgot/_next/static/FbFii31xjSVeyHLJ2RBOE/_buildManifest.js
+++ salgot/_next/static/FbFii31xjSVeyHLJ2RBOE/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/salgot//_next/:path+",
+        "destination": "/salgot/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
