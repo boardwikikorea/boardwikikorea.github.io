8d511cc1aeb3dbb2503ddd28e26858386589bd21
jangseokhwan <jinji0326@naver.com>
2026-09-22 07:58:40 +0900

    deploy: publish boardwiki web from main

---
+++ _next/static/chunks/00g8nv3ubjeim.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,67443,t=>{"use strict";var i=t.i(16891),e=t.i(8760),u=t.i(86844),a=t.i(65405);t.s(["default",0,function(){let{id:t}=(0,e.useParams)(),o=(0,a.useStaticRouteParam)(t,"id");return o?(0,i.jsx)(u.CatalogProductPage,{id:o}):null}])}]);
